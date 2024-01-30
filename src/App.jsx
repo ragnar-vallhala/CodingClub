e2accc5a1757923aeb22bb836713fc3084defb54
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-01-31 00:02:47 +0530

    bg changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import {Announcements, Rules, Billing, Business, CardDeal, Clients, CTA, Footer,
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">\
 
-
-  
-
-
-
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
