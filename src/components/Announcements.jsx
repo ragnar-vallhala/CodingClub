2b5c8666ae7307d4916b765612d3c51b9c297de7
chaittan <143733084+chaittan@users.noreply.github.com>
2024-02-18 15:07:26 +0530

    updated announcements

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -13,12 +13,11 @@ const Announcements = () => {
         
           <div className="scrolling-lines text-white font-big">
             <div className='scroll-box'>
-              <div className="line"> Registrations open till 8th Feb. </div>
-              <div className="line"> Participate in a hackathon promising a unique blend of CP and AI-Ml. </div>
+              <div className="line"> Editorial is out now! </div>
               <div className="line"> Prizes worth ₹60k up for grabs. </div>
               <div className="line"> Exciting news! We will soon be launching exclusive merchandise very soon. </div>
               <div className="line"> Participation certificate to all the participants. </div>
-              
+              <div className="line"> Editorial is out now! </div>
             </div>       
           </div>
     </section>
